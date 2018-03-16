2543959b8e024bf1f39f22bb0a1bb3a967500fb9
thylakoids <liyulongsjtu@hotmail.com>
2018-03-16 15:55:16 +0800

    add /src

---
+++ src/step0_downData.py
@@ -0,0 +1,6 @@
+import os
+
+if not os.exit('../lunadata/CSVFILE'):
+	mkdir('../lunadata/CSVFILE')
+if not os.exit('../lunadata/CSVFILE/annotations.csv'):
+	os.system('wget https://www.dropbox.com/sh/mtip9dx6zt9nb3z/AAANW5Vmi5IClbfh9bboGWwwa/annotations.csv?dl=0 ../lunadata/CSVFILE/annotations.csv')
